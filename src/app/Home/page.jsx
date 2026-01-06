efc3533731cca205719527ba972b7ebb0e880dd1
PrasanKumar1101 <prasan.kumar1101@gmail.com>
2026-01-06 15:18:55 +0530

    Added Home Page components and added logo text and button to home page

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+// import logo from "../../Assets/Images/images.jpeg";
 
 const HomePage = () => {
-  return <div>welcome to Home Page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-40 h-[100vh]">
+      <div className="h-40 w-40">
+        <img className="w-full h-full" alt="logo" src="/Images/images.jpeg" />
+      </div>
+      <div className="flex justify-center items-center gap-16 ">
+        <div className="w-2/4 text-center font-semibold basis-1/3">
+          <h1 className="text-white text-5xl">
+            BookShelf
+            <br />
+            <span className="text-warning">
+              Your personal libary and social network for bookworms
+            </span>
+          </h1>
+        </div>
+        <div>
+          <button className="btn btn-primary rounded-md px-5 py-3 text-2xl">
+            Register
+          </button>
+          <button className="btn btn-success btn-warning mx-3 rounded-md px-5 py-3 text-2xl">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
