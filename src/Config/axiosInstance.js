b9744d593385b182afc3cc13957c188bce214798
PrasanKumar1101 <prasan.kumar1101@gmail.com>
2026-01-09 16:48:59 +0530

    Fixed axios Instance

--- src/Config/axiosInstance.js
+++ src/Config/axiosInstance.js
@@ -2,7 +2,15 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  withCredentials: true,
+});
+
+// Add interceptor to attach token
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["x-access-token"] = token;
+  }
+  return config;
 });
 
 export default instance;
